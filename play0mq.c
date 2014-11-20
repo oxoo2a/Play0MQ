b258f7e50351b86968684c8decfdf5f7fbb35178
Asysob <sturm@uni-trier.de>
2014-11-20 16:47:34 +0100

    Determining roles
    Checking the first argument to be one of the 3 initial roles.

--- play0mq.c
+++ play0mq.c
@@ -3,17 +3,38 @@
 #include <unistd.h>
 #include <stdlib.h>
 #include <string.h>
+#include <stdbool.h>
+
+void usage (char *comment) {
+	fprintf(stderr,"usage: play0mq <role=source|broker|sink>\n");
+	if (comment != NULL)
+		fprintf(stderr,"       %s\n",comment);
+	exit(-1);
+}
+
+bool isRole ( char *role, char *value ) {
+	return strncmp(role,value,strlen(role)) == 0;
+}
+
+enum ROLE { SOURCE, BROKER, SINK };
+enum ROLE role;
 
 int main ( int ac, char **av ) {
-	if (ac < 2) {
-		fprintf(stderr,"%s <role=source|broker|sink>\n",av[0]);
-		exit(-1);
-	}
-
 	int major, minor, patch;
 	zmq_version (&major, &minor, &patch);
 	printf ("Current 0MQ version is %d.%d.%d\n", major, minor, patch);
-
+	
+	if (ac < 2) usage("At least a role must be specified");
+	
+	if (isRole("source",av[1]))
+		role = SOURCE;
+	else if (isRole("broker",av[1]))
+		role = BROKER;
+	else if (isRole("sink",av[1]))
+		role = SINK;
+	else
+		usage("Unkown role");
+		
 	void *context = zmq_ctx_new();
 
 	zmq_ctx_destroy(context);
