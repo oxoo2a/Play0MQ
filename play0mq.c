b080b796305f356fb4296e9062483e6fc3d071ef
Asysob <sturm@uni-trier.de>
2014-11-20 17:11:11 +0100

    Code templates for the 3 roles
    Preparing the structure for the 3 roles.

--- play0mq.c
+++ play0mq.c
@@ -6,6 +6,39 @@
 
 #include <zmq.h>
 
+//	********************************************************************************
+//	play0mq_source
+//	********************************************************************************
+void play0mq_source ( int ac, char **av ) {
+	void *context = zmq_ctx_new();
+	for (int i=0; i<ac; i++)
+		printf("%d = %s\n",i,av[i]);
+	zmq_ctx_destroy(context);
+}
+
+//	********************************************************************************
+//	play0mq_broker
+//	********************************************************************************
+void play0mq_broker ( int ac, char **av ) {
+	void *context = zmq_ctx_new();
+	for (int i=0; i<ac; i++)
+		printf("%d = %s\n",i,av[i]);
+	zmq_ctx_destroy(context);
+}
+
+//	********************************************************************************
+//	play0mq_sink
+//	********************************************************************************
+void play0mq_sink ( int ac, char **av ) {
+	void *context = zmq_ctx_new();
+	for (int i=0; i<ac; i++)
+		printf("%d = %s\n",i,av[i]);
+	zmq_ctx_destroy(context);
+}
+
+//	********************************************************************************
+//	main and some helper functions
+//	********************************************************************************
 void usage (char *comment) {
 	fprintf(stderr,"usage: play0mq <role=source|broker|sink>\n");
 	if (comment != NULL)
@@ -17,28 +50,21 @@ bool isRole ( char *role, char *value ) {
 	return strcmp(role,value) == 0;
 }
 
-enum ROLE { SOURCE, BROKER, SINK };
-enum ROLE role;
-
 int main ( int ac, char **av ) {
 	int major, minor, patch;
 	zmq_version (&major, &minor, &patch);
 	printf ("Current 0MQ version is %d.%d.%d\n", major, minor, patch);
-	
+
 	if (ac < 2) usage("At least a role must be specified");
 	
 	if (isRole("source",av[1]))
-		role = SOURCE;
+		play0mq_source(ac-2, &av[2]);
 	else if (isRole("broker",av[1]))
-		role = BROKER;
+		play0mq_broker(ac-2, &av[2]);
 	else if (isRole("sink",av[1]))
-		role = SINK;
+		play0mq_sink(ac-2, &av[2]);
 	else
 		usage("Unkown role");
-		
-	void *context = zmq_ctx_new();
-
-	zmq_ctx_destroy(context);
 
 	exit(0);
 }
